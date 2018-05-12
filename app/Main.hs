55eb981790edd909ff715737008be1d354a21399
luke <nerorevenge@gmail.com>
2018-05-12 00:51:45 -0400

    Code cleanup

--- app/Main.hs
+++ app/Main.hs
@@ -5,62 +5,14 @@
 module Main where
 
 import Lib
-import Diagrams.Prelude
-import Diagrams.TwoD.Polygons
 import Diagrams.Backend.SVG
+import Diagrams.Prelude
 import Diagrams.TwoD.Size
 
 
-rotate ::Int ->  [a] -> [a]
-rotate _ [] = []
-rotate n xs = zipWith const (drop n (cycle xs)) xs 
-
-
-sizE :: SizeSpec V2 Double
-sizE = dims2D 800 800
-
-
-rawPolygon :: Diagram B
-rawPolygon =  polygon $ PolygonOpts (PolyRegular 6 4) OrientV origin
-
-
-t :: [Diagram B]
-t =  map (\x -> rawPolygon # fc x) [black, white, red]
-
-
-topLeftHex :: Diagram B -> Diagram B
-topLeftHex = snugT . snugL
-
-
-topRightHex :: Diagram B -> Diagram B
-topRightHex = snugT . snugR
-
-
-xx :: Diagram B -> Diagram B
-xx = snugL . alignB 
-
-
-hexadd :: (Diagram B, Int) -> [Diagram B] -> (Diagram B, Int)
-hexadd (mempty, 0) x = (xx . hcat $ x , length x) 
-hexadd (top,pcount) bottom = let gradient = length bottom > pcount
-                                 ff = if gradient 
-                                         then topRightHex
-                                         else topLeftHex                                        
-                                 nbottom = hcat . map ff $ bottom
-                             in ((top <> nbottom) # xx, length bottom)
-
-
-gengen :: Int -> Int -> [[Diagram B]]
-gengen minm maxm = let a = [minm .. maxm] ++ reverse [minm..maxm-1]
-                       b = [0 .. (maxm - minm)]
-                       c = b ++ (reverse . init $ b)
-                   in map (\(x,y) -> take y . cycle $ Main.rotate x t) (zip c a)
-
-
-
-chessboard :: Diagram B
-chessboard = fst . foldl hexadd (mempty,0) $ gengen 34 50
+imageSize :: SizeSpec V2 Double
+imageSize = dims2D 800 800
 
 
 main :: IO ()
-main = renderSVG "hello2.svg" sizE chessboard
+main = renderSVG "hello2.svg" imageSize (genChessboard 37 45)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,59 @@
+{-# LANGUAGE NoMonomorphismRestriction #-}
+{-# LANGUAGE FlexibleContexts          #-}
+{-# LANGUAGE TypeFamilies              #-}
+
+
 module Lib
-    ( someFunc
+    ( genChessboard
     ) where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+
+import Diagrams.Backend.SVG
+import Diagrams.Prelude
+import Diagrams.TwoD.Polygons
+
+
+rotate ::Int ->  [a] -> [a]
+rotate _ [] = []
+rotate n xs = zipWith const (drop n (cycle xs)) xs 
+
+
+rawPolygon :: Diagram B
+rawPolygon =  polygon $ PolygonOpts (PolyRegular 6 4) OrientV origin
+
+
+coloredPolygonList :: [Diagram B]
+coloredPolygonList =  map (\x -> rawPolygon # fc x) [black, white, red]
+
+
+topLeftHex :: Diagram B -> Diagram B
+topLeftHex = snugT . snugL
+
+
+topRightHex :: Diagram B -> Diagram B
+topRightHex = snugT . snugR
+
+
+xx :: Diagram B -> Diagram B
+xx = snugL . alignB 
+
+
+hexadd :: (Diagram B, Int) -> [Diagram B] -> (Diagram B, Int)
+hexadd (mempty, 0) x = (xx . hcat $ x , length x) 
+hexadd (top,pcount) bottom = let gradient = length bottom > pcount
+                                 ff = if gradient 
+                                         then topRightHex
+                                         else topLeftHex                                        
+                                 nbottom = hcat . map ff $ bottom
+                             in ((top <> nbottom) # xx, length bottom)
+
+
+genComponents :: Int -> Int -> [[Diagram B]]
+genComponents minm maxm = let a = [minm .. maxm] ++ reverse [minm..maxm-1] 
+                              b = [0 .. (maxm - minm)] 
+                              c = b ++ (reverse . init $ b)
+                          in map (\(x,y) -> take y . cycle $ Lib.rotate x coloredPolygonList) (zip c a)
+
+
+genChessboard :: Int -> Int ->  Diagram B
+genChessboard minB maxB = fst . foldl hexadd (mempty,0) $ genComponents minB maxB
