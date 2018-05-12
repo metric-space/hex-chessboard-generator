54d0097c9338f0b203c541a37d607d25e2bd6aab
luke <nerorevenge@gmail.com>
2018-05-12 01:46:03 -0400

    Basic command line parsing

--- app/Main.hs
+++ app/Main.hs
@@ -5,14 +5,71 @@
 module Main where
 
 import Lib
+
+import Data.Semigroup ((<>))
 import Diagrams.Backend.SVG
-import Diagrams.Prelude
+import Diagrams.Prelude hiding (option, value)
 import Diagrams.TwoD.Size
+import Options.Applicative 
 
 
-imageSize :: SizeSpec V2 Double
-imageSize = dims2D 800 800
+data Inputs = Inputs
+  { fileName    :: String
+  , imageWidth  :: Int
+  , imageHeight :: Int
+  , chessMinC   :: Int
+  , chessMaxC   :: Int }
+
+
+inputs :: Parser Inputs
+inputs = Inputs
+      <$> strOption
+          ( long "output-svg-file-name"
+         <> metavar "FILE"
+         <> help "Output name of generated svg file" 
+         <> value "hex_chessboard.svg"
+         <> showDefault)
+      <*> option auto
+          ( long "imageWidth"
+         <> help "output image width"
+         <> short 'w'
+         <> showDefault
+         <> value 800
+         <> metavar "INT")
+      <*> option auto
+          ( long "imageHeight"
+         <> help "output image height"
+         <> short 'h'
+         <> showDefault
+         <> value 800
+         <> metavar "INT")
+      <*> option auto
+          ( long "min-side-count"
+         <> help "hex chessboard min side count"
+         <> short '1'
+         <> showDefault
+         <> value 3
+         <> metavar "INT")
+      <*> option auto
+          ( long "max-side-count"
+         <> help "hex chessboard max side count"
+         <> short '2'
+         <> showDefault
+         <> value 10
+         <> metavar "INT")
 
 
 main :: IO ()
-main = renderSVG "hello2.svg" imageSize (genChessboard 37 45)
+main = genHexChessBoard =<< execParser opts
+  where
+    opts = info (inputs <**> helper)
+      ( fullDesc
+     <> progDesc "generates a hexagonal chessboard image using the mighty power of the Diagrams Lib (cue Yorgey Rap)"
+     <> header "Hexagonal ChessBoard Image Generator " )
+
+
+genHexChessBoard :: Inputs -> IO ()
+genHexChessBoard (Inputs fileName w h cmin cmax) = let [wd, hd] = map fromIntegral [w, h] 
+                                                       imageSize = dims2D wd hd :: SizeSpec V2 Double
+                                                   in renderSVG fileName imageSize (genChessboard cmin cmax)
+                                                                            
